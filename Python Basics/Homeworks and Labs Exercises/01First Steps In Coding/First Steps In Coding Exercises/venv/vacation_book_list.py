b542eb86bf12f290bdc9fd4c0c4c4df8fc72e948
Stanislav Krastev <stanis.krastev@gmail.com>
2022-05-27 00:18:27 +0200

    Delete Python Basics/Homeworks and Labs Exercises/01First Steps In Coding/First Steps In Coding Exercises/venv directory

--- Python Basics/Homeworks and Labs Exercises/01First Steps In Coding/First Steps In Coding Exercises/venv/vacation_book_list.py
+++
@@ -1,7 +0,0 @@
-pages = int(input("Put pages number: "))
-pages_hour = int(input("Put pages per hour: "))
-number_days = int(input("Put number of days: "))
-
-time_per_book = pages / pages_hour
-hours_needed = time_per_book / number_days
-print (f"We need this amount of hours per day to read the pages:  {hours_needed}")
