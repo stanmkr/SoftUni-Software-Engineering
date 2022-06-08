e289d5ae9ed184eb8720cfc852351ead7afeb61d
Stanislav Krastev <stanis.krastev@gmail.com>
2022-06-08 12:20:37 +0200

    adding exercise

---
+++ Java Basics/Homeworks and Labs Exercises/01First Steps In Coding/First Steps In Coding Lab/GreetingByName.java
@@ -0,0 +1,11 @@
+package JAVA.PROGRAMMING_BASICS.First_Steps_In_Coding.LAB;
+
+import java.util.Scanner;
+
+public class GreetingByName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = (scanner.nextLine());
+        System.out.println("Hello, " + name + "!");
+    }
+}
