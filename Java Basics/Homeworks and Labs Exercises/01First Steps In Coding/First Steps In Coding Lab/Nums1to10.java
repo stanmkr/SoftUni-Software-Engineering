b27297c6ea10d2379063baef9715fb6f0cb83704
Stanislav Krastev <stanis.krastev@gmail.com>
2022-06-10 09:07:45 +0200

    Adding exercise

---
+++ Java Basics/Homeworks and Labs Exercises/01First Steps In Coding/First Steps In Coding Lab/Nums1to10.java
@@ -0,0 +1,8 @@
+package JAVA.PROGRAMMING_BASICS.First_Steps_In_Coding.LAB;
+
+public class Nums1to10 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++)
+            System.out.println(i);
+    }
+}
