71203b019a118fe5e41d4c965c8623c25f7c5652
Stanislav Krastev <stanis.krastev@gmail.com>
2022-06-20 11:19:43 +0200

    Update BeerKegs.java

--- Java Fundamentals/Homeworks and Labs Exercises/01Data Types and Variables/Data Types and Variables Exercises/BeerKegs.java
+++ Java Fundamentals/Homeworks and Labs Exercises/01Data Types and Variables/Data Types and Variables Exercises/BeerKegs.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Data_Types_and_Variables_exercises;
 
 import java.util.Locale;
@@ -31,37 +30,3 @@ public class beer_kegs {
         System.out.println(maxModel);
     }
 }
-=======
-package Data_Types_and_Variables_exercises;
-
-import java.util.Locale;
-import java.util.Scanner;
-
-/**
- * Created by Stanislav Krastev
- * Data_Types_and_Variables - Data_Types_and_Variables_exercises
- * Date: May / 2022
- */
-public class beer_kegs {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
-        double mayor = Double.MIN_VALUE;
-        int numberOfLines = Integer.parseInt(scanner.nextLine());
-        String maxModel = "";
-
-        for (int i = 0; i < numberOfLines; i++) {
-            String nombre = scanner.nextLine();
-            double radio = Double.parseDouble(scanner.nextLine());
-            int altura = Integer.parseInt(scanner.nextLine());
-            double volume = Math.PI * Math.pow(radio, 2) * altura;
-            if (volume > mayor) {
-                mayor = volume;
-                maxModel = nombre;
-
-            }
-
-        }
-        System.out.println(maxModel);
-    }
-}
->>>>>>> fedb860a8a55910e2d6b4515736a339e3c8faf1f
